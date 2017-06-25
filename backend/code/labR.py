97eb9cd339e202de37d8d82b601c8edf570b529f
AAT <aman.iitk072@gmail.com>
2017-06-25 09:43:51 +0530

    labR class added.

---
+++ backend/code/labR.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+import sqlite3
+
+class labR:
+	'''
+	'''
+	
+	__init__(self):
+		'''
+		'''
+		
+		#Declaration of the variables.
+		_project_title = None
+		_purpose       = []
+		_materials     = []
+		_theory        = None
+		_procedures    = None
+		
+		return
+	pass
