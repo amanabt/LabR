73719b29fb0d28f3e889de22db94c340c8101861
AAT <aman.iitk072@gmail.com>
2017-06-25 10:41:28 +0530

    Temporary commit.

--- backend/code/labR.py
+++ backend/code/labR.py
@@ -1,21 +1,107 @@
 #! /usr/bin/env python3
 
 import sqlite3
+import copy
 
-class labR:
+class Material:
 	'''
 	'''
 	
-	__init__(self):
+	def __init__(self):
+		'''
+		'''
+		self._material = str()
+		self._notes    = str()
+		
+		return
+	pass
+
+class Observation:
+	'''
+	'''
+	
+	def __init__(self):
+		'''
+		'''
+		self._data   = []
+		self._images = []
+		self._videos = []
+		
+		return
+	
+	pass
+
+class Procedure_step:
+	'''
+	'''
+	
+	def __init__(self):
+		'''
+		'''
+		self._step        = str()
+		self._observation = []
+		self._cautions    = []
+		
+		return
+	
+	pass
+
+
+class LabR:
+	'''
+	'''
+	
+	def __init__(self):
 		'''
 		'''
 		
 		#Declaration of the variables.
-		_project_title = None
-		_purpose       = []
-		_materials     = []
-		_theory        = None
-		_procedures    = None
+		
+		self._start_time    = None
+		self._end_time      = None
+		self._project_title = str()
+		self._purpose       = []
+		self._materials     = []
+		self._theory        = str()
+		self._procedures    = str()
 		
 		return
+	
+	def addProjectTitle(self, project_title):
+		'''
+		'''
+		self._project_title = project_title
+		return
+
+	def add_purpose(self, purpose):
+		'''
+		'''
+		self._purpose.append(purpose)
+		return
+	
+	def add_material(self, material):
+		'''
+		'''
+		self._materials.append(material)
+		return
+	
+	def add_theory(self, theory):
+		'''
+		'''
+		self._theory = theory
+		return
+	
+	
 	pass
+
+if __name__ == '__main__':
+	'''
+	'''
+	
+	labr1 = LabR()
+	
+	labr1.addProjectTitle(input('Enter Project Title:'))
+	labr1.add_purpose(input('Enter Purpose:'))
+	
+	return
+
