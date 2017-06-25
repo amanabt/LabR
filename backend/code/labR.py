06ce9bfcef0e77c3ce3237498304af27aa2719ad
AAT <aman.iitk072@gmail.com>
2017-06-25 11:36:48 +0530

    Temporary commit.

--- backend/code/labR.py
+++ backend/code/labR.py
@@ -2,17 +2,33 @@
 
 import sqlite3
 import copy
+from datetime import datetime as dt
+from datetime import timedelta
+
+#[TODO] Implement auto-save function
 
 class Material:
 	'''
 	'''
 	
-	def __init__(self):
+	def __init__(self, material = str()):
 		'''
 		'''
-		self._material = str()
-		self._notes    = str()
+		self._material = material
+		self._notes    = []
 		
+		return
+	
+	def add_material(self, material):
+		'''
+		'''
+		self._material = material
+		return
+	
+	def add_note(self, note):
+		'''
+		'''
+		self._notes.append(note)
 		return
 	pass
 
@@ -31,17 +47,30 @@ class Observation:
 	
 	pass
 
-class Procedure_step:
+class Procedure:
 	'''
 	'''
 	
-	def __init__(self):
+	def __init__(self, step = None):
 		'''
 		'''
 		self._step        = str()
 		self._observation = []
 		self._cautions    = []
 		
+		return
+	
+	def add_step(self, step):
+		'''
+		'''
+		self._step = step
+		
+		return
+	
+	def add_observation(self, observation):
+		'''
+		'''
+		self._observation.append(observation)
 		return
 	
 	pass
@@ -54,18 +83,34 @@ class LabR:
 	def __init__(self):
 		'''
 		'''
-		
 		#Declaration of the variables.
 		
-		self._start_time    = None
-		self._end_time      = None
+		self._start_time         = None
+		self._end_time           = None
+		self._time_of_experiment = None
+		
 		self._project_title = str()
 		self._purpose       = []
 		self._materials     = []
 		self._theory        = str()
-		self._procedures    = str()
+		self._procedures    = []
 		
 		return
+	
+	def start_experiment(self):
+		'''
+		'''
+		self._start_time = dt.now()
+		
+		return
+	
+	def end_experiment(self):
+		'''
+		'''
+		self._end_time = dt.now()
+		self._time_of_experiment = self._start_time - self._end_time
+		return
+	
 	
 	def addProjectTitle(self, project_title):
 		'''
@@ -91,6 +136,45 @@ class LabR:
 		self._theory = theory
 		return
 	
+	def add_theory(self, theory):
+		'''
+		'''
+		self._theory = theory
+		
+		return
+	
+	def add_new_procedure(self, step = None):
+		'''
+		'''
+		self._procedures.append(Procedure())
+		
+		return
+	
+	def addProcedureStep(self, procedure_idx, step):
+		'''
+		'''
+		
+		self._procedures[procedure_idx].add_step(step)
+		return
+	
+	def addProcedureObservation(self, procedure_idx, observation):
+		'''
+		'''
+		self._procedures[procedure_idx].add_observation(observation)
+		return
+	
+	def recordImageObservation(self):
+		'''
+		'''
+		print('Yet to be implemented!')
+		return
+	
+	def save(self):
+		'''
+		'''
+		#[TODO]: Save the lab report.
+		print('Yet to be implemented!')
+		return
 	
 	pass
 
@@ -100,8 +184,24 @@ if __name__ == '__main__':
 	
 	labr1 = LabR()
 	
-	labr1.addProjectTitle(input('Enter Project Title:'))
-	labr1.add_purpose(input('Enter Purpose:'))
+	labr1.start_experiment()
+	labr1.addProjectTitle(input('Enter Project Title: '))
+	labr1.add_purpose(input('Enter Purpose: '))
 	
-	return
+	print('Press 1 to add material, to stop adding material, press 0: ')
+	
+	while(int(input()) == 1):
+		material = Material(input('Enter Material Details: '))
+		note     = input('Enter some note: ')
+		material.add_note(note)
+		
+		labr1.add_material(material)
+		pass
+	
+	print('Press 1 to add Procedure, to stop adding procedure, press 0: ')
+	
+	while(int(input()) == 1):
+		step = input('Enter New step: ')
+		labr1.add_new_procedure(step)
+		
 
